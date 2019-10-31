6605f6ead2eaedc3fa478bb899e9f2476684742b
jeehmilena <jessicamilenacorrea@gmail.com>
2019-10-30 21:42:05 -0300

    Updates on BottomNavigation

---
+++ app/src/main/java/br/com/movieapp/view/activity/HomeActivity.java
@@ -0,0 +1,28 @@
+package br.com.movieapp.view.activity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import br.com.movieapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_movie, R.id.navigation_favorite, R.id.navigation_notifications)
+                .build();
+    }
+
+}
--- app/src/main/java/br/com/movieapp/view/activity/SplashActivity.java
+++ app/src/main/java/br/com/movieapp/view/activity/SplashActivity.java
@@ -2,15 +2,33 @@ package br.com.movieapp.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import br.com.movieapp.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                jump();
+            }
+        }, 3000);
+    }
+
+    private void jump() {
+        timer.cancel();
+        startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+        finish();
     }
 }
