55262bc0b88fca121c0ade9880336b2156bcff7b
jeehmilena <jessicamilenacorrea@gmail.com>
2019-10-30 21:24:05 -0300

    initial commit

---
+++ app/src/main/java/br/com/movieapp/view/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package br.com.movieapp.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.movieapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
